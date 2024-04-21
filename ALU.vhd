df0807ba1fa9e2caa5e3eca6fe3c9e3a45ac2482
theHeliE <129796088+theHeliE@users.noreply.github.com>
2024-04-22 00:46:42 +0200

    Fixes

--- ALU.vhd
+++ ALU.vhd
@@ -27,10 +27,11 @@ begin
             A when "0100",
             B when "0101",
             A + B when "0110",
+            A + 1 when "0111",
             (others => '0') when others;
         carry <= '1' when (sel="0110" and ((A >= 0 and B > to_signed(2147483647, 32) - A) or
             (B >= 0 and A > to_signed(2147483647, 32) - B))) or
-            (sel="0010" and unsigned(A) < unsigned(B)) else '0';
+            (sel="0010" and unsigned(A) < unsigned(B)) or (sel="0111" and ((A >= 0 and A > to_signed(2147483647, 32) - 1))) else '0';
         zero <= '1' when alu_output_int = "00000000000000000000000000000000" else '0';
         overflow <= '1' when ((A(31) = B(31)) and (A(31) /= alu_output_int(31))) else '0';       
         negative <= alu_output_int(31);
